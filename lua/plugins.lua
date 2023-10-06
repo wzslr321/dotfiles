210f5381024c6dda94c8b0f04d088fad46449333
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-10-06 11:22:39 +0200

    some updates idk

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -54,16 +54,24 @@ require("lazy").setup({
 		'nvim-tree/nvim-tree.lua'
 	},
 	{ 'akinsho/toggleterm.nvim', version = "*", opts = {} },
+
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 })
 
 require("toggleterm").setup {}
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = {  "lua", "vim", "vimdoc", "query", "wing" },
+	ensure_installed = { "lua", "vim", "vimdoc", "query", "wing" },
 
 	sync_install = false,
 	auto_install = true,
-	ignore_install =  {"javascript"},
+	ignore_install = { "javascript" },
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
